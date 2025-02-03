d2bf4e4202f1a7e92e193b0abd04b09d58022a86
shaikhsameershabbir <samsk7774@gmail.com>
2025-02-03 20:02:33 +0530

    express server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
-function login(username: string) {
-    console.log(`User ${username} logged in`);
+import app from './app';
+import { Config } from './config';
 
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 }
-login('admin');
+
+startServer();
+
